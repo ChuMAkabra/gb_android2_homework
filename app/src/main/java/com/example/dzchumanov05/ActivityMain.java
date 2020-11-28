75c4ead8b1f16305eb34d8f2f31356e9869f8fc0
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-11-28 13:08:13 +0300

    2) Added Drawer + FAB + an additional toolbar (the main toolbar wasn't hidden yet): -DrawerLayout added: --CoordinatorLayout ---Toolbar -----menu added (via app:menu tag) ----Image (app icon) ---FloatingActionButton --NavigationView ---header added (via app:headerLayout tag) ----background gradient added (via android:background tag) ----Image (app icon) ----TextView (app name) ---menu added (via app:menu tag)
    -corresponding menus created
    -onCreate method created setting drawer layout via setContentView
    -strings(ru) added
    -style added and included into Manifest
    
    TODO:
    -add a container into CoordinatorLayout to further include fragments into it.
    -add actions to all menu items
    -highlight navigation view item once chosen

--- app/src/main/java/com/example/dzchumanov05/ActivityMain.java
+++ app/src/main/java/com/example/dzchumanov05/ActivityMain.java
@@ -1,7 +1,14 @@
 package com.example.dzchumanov05;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityMain extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_layout);
+    }
 }
