b36b5c682deab3def6bd2aa1b9b3ab0a4f054f6d
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-12-02 22:49:24 +0300

    3) The default toolbar hidden. The new one shown. -ActivityMain.java: onCreate() updated, onCreateOptionsMenu created -toolbar_menu.xml: "preferences" and "about" Icons hidden in the Options View as the search icon is changeable depending on the theme selected -themes.xml: 3 styles created for the main activity, App Bar and Options View overlays

--- app/src/main/java/com/example/dzchumanov05/ActivityMain.java
+++ app/src/main/java/com/example/dzchumanov05/ActivityMain.java
@@ -1,14 +1,52 @@
 package com.example.dzchumanov05;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class ActivityMain extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_layout);
+
+        // устанавливаем тулбар в Action Bar
+        Toolbar toolbar = initToolbar();
+        // создаем кнопку-"сэндвич", открывающую боковую панель (навигационное меню)
+        initDrawerToggle(toolbar);
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    private void initDrawerToggle(Toolbar toolbar) {
+        DrawerLayout drawer = findViewById(R.id.drawer_container);
+        try {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        // создаем слушатель, который будет обрабатывать открытие и закрытие навигационного меню
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        // добавляем его к навигационному меню
+        drawer.addDrawerListener(toggle);
+        // синхронизируем их
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
